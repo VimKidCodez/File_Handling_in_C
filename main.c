fd0efab2bf07d3bc78d1a74dddbc3e0f17cd74c5
S D Sriram <sdsriram1@gmail.com>
2022-05-10 19:29:09 +0530

    -

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <string.h>
+
+// stream oriented datda file - the datA si stored the same manner it is appers on the screen
+
+// sytem oriented data file - the data files are more closely associated with the operating system
+
+// c file operations
+//declare a file,reading,writing data in a file and closing it
+	
+// Steps to porcces a file 
+//Declare a file pointer variable 
+//Open a file using fopen() funtion
+//Process the file using a suitable funtion
+//Close the filoe using fclose() funtion
+
+//Function
+// getc - to read characters for  file
+//putc- To write a character in a file 
+//getw - to get intergs in a file
+// putw - write an interger in a file
+// fprintf - Prints formatted output  into a file
+// fscanf - Reads formatted input from a file
+// fgets - To read strings for a character in a file
+// fputs - Write a string of charaters into a file
+// feof - Detects file marker in file
+
+void 
+
+
+
+int main(){
+
+	
+
+
+	return 0 ;
+}
